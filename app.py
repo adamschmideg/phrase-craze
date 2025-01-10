13dc7c51c9c65659f2a3f53c63e25cd7ed9ef8ca
Adam Schmideg <adamschmideg@users.noreply.github.com>
2025-01-10 12:59:18 +0100

    Show question and answers

--- app.py
+++ app.py
@@ -5,46 +5,55 @@ app = Flask(__name__)
 
 # Sample data for quizzes and questions
 quizzes = {
-    1: {
+    0: {
         'title': 'Phrase Identification Quiz',
         'questions': {
-            1: {
-                'phrase': 'The quick brown fox jumps over the lazy dog.',
+            0: {
+                'question_id': 0,
+                'text': 'The quick brown fox jumps over the lazy dog.',
                 'answers': [
-                    'The quick brown fox jumps over the lazy dog.',
-                    'A quick brown fox jumps over a lazy dog.',
-                    'The quick brown fox jumped over the lazy dog.',
-                    'The quick brown fox jumps over the lazy cat.'
+                    {'answer_id': 0, 'text': 'jumps over the lazy dog.', 'correct': True},
+                    {'answer_id': 1, 'text': 'jumps over the dog!', 'correct': False},
+                    {'answer_id': 2, 'text': 'jumps over a lazy dog.', 'correct': False},
+                    {'answer_id': 3, 'text': 'jumps over the lazy cat.', 'correct': False}
                 ]
             },
-            2: {
-                'phrase': 'A journey of a thousand miles begins with a single step.',
+            1: {
+                'question_id': 2,
+                'text': 'A stitch in time saves nine.',
                 'answers': [
-                    'A journey of a thousand miles begins with a single step.',
-                    'A journey of one thousand miles begins with a single step.',
-                    'A journey of a thousand miles starts with one step.',
-                    'A journey of many miles begins with one step.'
+                    {'answer_id': 0, 'text': 'saves nine.', 'correct': True},
+                    {'answer_id': 1, 'text': 'saves time.', 'correct': False},
+                    {'answer_id': 2, 'text': 'saves a stitch.', 'correct': False},
+                    {'answer_id': 3, 'text': 'saves a stitch in time.', 'correct': False}
                 ]
-            }
-            # Add more questions as needed
+            },
         }
     }
 }
 
+def convert_nested_dict_to_object(d):
+    class ObjectView(object):
+        def __init__(self, d):
+            self.__dict__ = d
+    return ObjectView(d)
+
 @app.route('/')
 def home():
-    return redirect(url_for('question', quiz_id=1, question_id=1))
+    return redirect(url_for('show_question', quiz_id=0, question_id=0))
 
 @app.route('/quiz/<int:quiz_id>/question/<int:question_id>')
-def question(quiz_id, question_id):
+def show_question(quiz_id, question_id):
     question = quizzes[quiz_id]['questions'][question_id]
-    return render_template('question.html', question=question, question_id=question_id, quiz_id=quiz_id)
+    answer_ids = ','.join(str(answer['answer_id']) for answer in question['answers'])
+    answers_url = url_for('show_answers', quiz_id=quiz_id, question_id=question_id, answer_ids=answer_ids)
+    return render_template('question.html', question=question, answers_url=answers_url)
 
-@app.route('/quiz/<int:quiz_id>/question/<int:question_id>/answers/<ids>')
-def answers(quiz_id, question_id, ids):
+@app.route('/quiz/<int:quiz_id>/question/<int:question_id>/answers/<answer_ids>')
+def show_answers(quiz_id, question_id, answer_ids):
     question = quizzes[quiz_id]['questions'][question_id]
-    answers = [question['answers'][int(i)] for i in ids.split(',')]
-    return render_template('answers.html', question=question['phrase'], answers=answers)
+    answers = [question['answers'][int(i)] for i in answer_ids.split(',')]
+    return render_template('answers.html', question=question, answers=answers)
 
 @app.route('/quiz/<int:quiz_id>/question/<int:question_id>/answer/<int:answer_id>', methods=['POST'])
 def submit_answer(quiz_id, question_id, answer_id):
