583ca57f0ad64c502a815e791093de58d640490b
Adam Schmideg <adamschmideg@users.noreply.github.com>
2025-01-13 17:07:35 +0100

    Create empty models

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+    difficulty = models.IntegerField(default=1)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text = models.TextField()
+    is_correct = models.BooleanField()
+    difficulty = models.IntegerField(default=1)
+
+class Status(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    question_difficulty = models.IntegerField()
+    answer_difficulty = models.IntegerField()
+    match_in_round_index = models.IntegerField()
