1fae791b8e75e9076831550a4cb0db9d636429a7
Adam Schmideg <adamschmideg@users.noreply.github.com>
2025-01-14 11:34:02 +0100

    Update status with matches played and won

--- quiz/models.py
+++ quiz/models.py
@@ -1,8 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 from random import randint
 
+class StatusManager(models.Manager):
+    def get_or_create(self, user):
+        user = user if user.is_authenticated else User.objects.get(username='placeholder')
+        try:
+            return self.get(user=user)
+        except self.model.DoesNotExist:
+            return self.create(user=user)
+
+class Status(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    question_difficulty = models.IntegerField(default=1)
+    answer_difficulty = models.IntegerField(default=1)
+    matches_played = models.IntegerField(default=0)
+    matches_won = models.IntegerField(default=0)
+    matches_per_round = models.IntegerField(default=5)
+    objects = StatusManager()
+
+    def increase_difficulty(self):
+        pass
+
+    def decrease_difficulty(self):
+        pass
+
 class QuestionManager(models.Manager):
-    def random_for_status(self, status):
+    def random_question(self, status):
         try:
             question_count = self.filter(difficulty=status.question_difficulty).count()
             if question_count == 0:
@@ -22,10 +46,3 @@ class Answer(models.Model):
     text = models.TextField()
     is_correct = models.BooleanField(default=False)
     difficulty = models.IntegerField(default=1)
-
-class Status(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    question_difficulty = models.IntegerField(default=1)
-    answer_difficulty = models.IntegerField(default=1)
-    matches_played = models.IntegerField(default=0)
-    matches_per_round = models.IntegerField(default=5)
--- quiz/views.py
+++ quiz/views.py
@@ -1,20 +1,24 @@
 import random
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.forms.models import model_to_dict
+
 from quiz import models
-from django.contrib.auth.models import User
+
+PROMOTE_AT_RATIO = 0.8
+DEMOTE_AT_RATIO = 0.5
 
 def home(request):
-    user = request.user if request.user.is_authenticated else User.objects.get(username='placeholder')
-    status = models.Status.objects.get_or_create(user=user)[0]
-    question = models.Question.objects.random_for_status(status)
+    status = models.Status.objects.get_or_create(request.user)
+    question = models.Question.objects.random_question(status)
     return redirect(reverse('show_question', kwargs={'quiz_id': 0, 'question_id': question.id}))
 
 def show_question(request, quiz_id, question_id):
+    status = model_to_dict(models.Status.objects.get_or_create(request.user))
     question = models.Question.objects.get(id=question_id)
     answer_ids = ','.join([str(answer.id) for answer in question.answers.all()])
     answers_url = reverse('show_answers', kwargs={'quiz_id': quiz_id, 'question_id': question_id, 'answer_ids': answer_ids})
-    return render(request, 'quiz/question.html', {'question': question, 'answers_url': answers_url})
+    return render(request, 'quiz/question.html', {'question': question, 'answers_url': answers_url, 'status': status})
 
 def show_answers(request, quiz_id, question_id, answer_ids):
     answers = models.Answer.objects.filter(id__in=answer_ids.split(','))
@@ -23,11 +27,20 @@ def show_answers(request, quiz_id, question_id, answer_ids):
 def submit_answer(request, quiz_id, question_id, answer_id):
     if request.method == 'POST':
         answer = models.Answer.objects.get(id=answer_id)
-        if answer.is_correct:
-            # TODO: Score
-            pass
+        status = models.Status.objects.get_or_create(request.user)
 
-        question_count = models.Question.objects.count()
-        next_question_id = random.randint(0, question_count - 1)
-        return redirect(reverse('show_question', kwargs={'quiz_id': quiz_id, 'question_id': next_question_id}))
+        status.matches_played += 1
+        if answer.is_correct:
+            status.matches_won += 1
+        if status.matches_played == status.matches_per_round:
+            if status.matches_won / status.matches_per_round >= PROMOTE_AT_RATIO:
+                status.increase_difficulty()
+            elif status.matches_won / status.matches_per_round <= DEMOTE_AT_RATIO:
+                status.decrease_difficulty()
+            status.matches_won = 0
+            status.matches_played = 0
+        status.save()
+
+        next_question = models.Question.objects.random_question(status)
+        return redirect(reverse('show_question', kwargs={'quiz_id': quiz_id, 'question_id': next_question.id}))
 
