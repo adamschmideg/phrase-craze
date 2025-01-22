2637ad667b3c9f37a7815b724801d3f3f918a582
Adam Schmideg <adamschmideg@users.noreply.github.com>
2025-01-22 16:35:01 +0100

    Failing test for skipping missing difficulty

--- quiz/tests.py
+++ quiz/tests.py
@@ -32,4 +32,25 @@ class PrepareShowAnswersTest(TestCase):
         next_question = Question.objects.get(pk=context['question_id'])
         self.assertEqual(next_question.difficulty, 2)
 
+    def test_skip_missing_difficulty(self):
+        question1 = Question.objects.create(text="Question 1", difficulty=1)
+        answer1 = Answer.objects.create(question_id=question1.id, text="Answer 1", is_correct=True)
+
+        question2 = Question.objects.create(text="Question 1", difficulty=3)
+
+        status = Status.objects.get_or_create(self.user)
+        status.question_difficulty = 1
+        status.matches_per_round = 1
+        status.save()
+
+        request = self.factory.get('/dummy/')
+        request.user = self.user
+        request.method = 'POST'
+
+        quiz_id = 1
+        template_name, context = views.prepare_submit_answer(request, quiz_id, question1.id, answer1.id)
+
+        next_question = Question.objects.get(pk=context['question_id'])
+        self.assertEqual(next_question.difficulty, question2.difficulty)
+
 
