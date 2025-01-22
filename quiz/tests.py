62a54b7fdd1ebaad92ade8d1faafd63bb203cf9f
Adam Schmideg <adamschmideg@users.noreply.github.com>
2025-01-22 16:24:46 +0100

    Simplest test case with increased difficulty works

--- quiz/tests.py
+++ quiz/tests.py
@@ -13,13 +13,14 @@ class PrepareShowAnswersTest(TestCase):
 
     def test_prepare_show_answers(self):
         question1 = Question.objects.create(text="Question 1", difficulty=1)
-        answer1 = Answer.objects.create(question_id=question1.id, text="Answer 1")
+        answer1 = Answer.objects.create(question_id=question1.id, text="Answer 1", is_correct=True)
 
         question2 = Question.objects.create(text="Question 1", difficulty=2)
         answer2 = Answer.objects.create(question_id=question2.id, text="Answer 1")
 
         status = Status.objects.get_or_create(self.user)
         status.question_difficulty = 1
+        status.matches_per_round = 1
         status.save()
 
         request = self.factory.get('/')
@@ -30,9 +31,7 @@ class PrepareShowAnswersTest(TestCase):
 
         # Call the prepare function
         quiz_id = 1
-        question_id = 1
-        answer_id = 1
-        template_name, context = views.prepare_submit_answer(request, quiz_id, question_id, answer_id)
+        template_name, context = views.prepare_submit_answer(request, quiz_id, question1.id, answer1.id)
 
         next_question = Question.objects.get(pk=context['question_id'])
         self.assertEqual(next_question.difficulty, 2)
